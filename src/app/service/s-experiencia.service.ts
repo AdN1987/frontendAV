c7cfcf968b5987c3b40a4f8a9d717bc8d8e9621b
Adrian Villa <adrian.villa@live.com.ar>
2023-05-11 22:16:16 -0300

    se agrego enviromens

--- src/app/service/s-experiencia.service.ts
+++ src/app/service/s-experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from "../model/Experiencia";
 })
 
 export class SExperienciaService {
-  expURL = 'http://localhost:8080/explab/'
+  expURL = 'https://backendav.onrender.com/explab/'
 
   constructor(private httpClient: HttpClient) { }
 
---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+
+  production: false,
+
+  URL : 'https://backendav.onrender.com/',
+ // URL: 'http://localhost:8080/'
+};
